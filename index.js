5839b1c611d38c03ac76cf4c463f764361c5fa25
Mikaël Sévigny <sevignator@hotmail.com>
2023-12-27 07:36:24 -0500

    Add the CLI argument and options

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Test');
+import { Option, program } from 'commander';
+
+program
+  .argument('<component-name>', 'new component name')
+  .option('--typescript', 'create files using the `.ts` and `.tsx` extensions')
+  .addOption(
+    new Option('--styling <choice>', 'specify a styling library').choices([
+      'vanilla-extract',
+    ])
+  );
+
+program.parse();
+
+const componentName = program.args[0];
+const options = program.opts();
+
+console.log({
+  componentName,
+  options,
+});
